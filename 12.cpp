d3985cfbc74c4ff0e7bc4e23f04f0e89e9309bc6
Spoorthi101 <143537838+Spoorthi101@users.noreply.github.com>
2023-10-03 16:58:43 +0530

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+	int i,j;
+	for (i=1;i<=5;i++)
+	{
+		for (j=1;j<=6-i;j++)
+		printf("* ");
+		printf("\n");
+	}
+		
+	for (i=1;i<=5;i++)
+	{
+		for (j=1;j<=i;j++)
+		printf("* ");
+	
+     	printf("\n");
+    }
+}
